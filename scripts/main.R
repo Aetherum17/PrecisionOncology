f86a86354e48385c92f5d7aa580d369d84e004d9
Aetherum <dl1703@mail.ru>
2023-04-04 15:02:46 +0100

    work

--- scripts/main.R
+++ scripts/main.R
@@ -12,13 +12,16 @@ library(rstudioapi)
 library(R.utils)
 library(tools)
 library(readr)
+library(htmltools)
+library(htmlTable)
+library(shiny)
 
 # Functions ####################################################################
 
 # This functions tries to find potential targets in the list of mutations for each sample
 find_applicable_drugs <- function(data_drugs, samples_df){
   # Create an output data frame
-  patients_df <- data.frame('Sample ID' = "X", 'Diagnosis' = 'Q', 'Target' = "Y", 'Drug' = 'Z')
+  patients_df <- data.frame('Sample_ID' = "Z", 'Diagnosis' = 'X', 'Target' = "C", 'Drug' = 'V', 'Found_Drug' = 'B')
   
   # For each SAMPLE
   for(i in 1:nrow(samples_df)){
@@ -30,12 +33,12 @@ find_applicable_drugs <- function(data_drugs, samples_df){
       # Check if a drug is in the mutations list
       if(data_drugs[q, 2] %in% mutations) {
         # if it is there - add information about the finding
-        patients_df <- rbind(patients_df, c(samples_df[i,1], samples_df[i,2], data_drugs[q, 2], data_drugs[q, 1]))
+        patients_df <- rbind(patients_df, c(samples_df[i,1], samples_df[i,2], data_drugs[q, 2], data_drugs[q, 1], TRUE))
         found_drug = T
       }
     }
     if(found_drug == F){
-      patients_df <- rbind(patients_df, c(samples_df[i,1], samples_df[i,2], "---", "---"))
+      patients_df <- rbind(patients_df, c(samples_df[i,1], samples_df[i,2], "---", "---", FALSE))
     }
   }
   patients_df <- patients_df[-1,]
@@ -53,8 +56,12 @@ setwd(directory)
 # Output and log files
 outputfile_path <- paste(directory, "results/output.txt", sep = "/")
 logfile_path <- paste(directory, "results/log.log", sep = "/")
-file.create(outputfile_path)
+summary_path <- paste(directory, "results/summary.html", sep = "/")
+#summary_path <- paste(directory, "results/summary.txt", sep = "/")
+#file.create(outputfile_path)
+#write(c("Sample_ID;Diagnosis;Target;Drug;Found_Drug"), outputfile_path, append=TRUE)
 file.create(logfile_path)
+file.create(summary_path)
 unlink(paste(directory, "data/temp", sep = "/"), recursive = TRUE)
 
 # Drugs Data
@@ -90,7 +97,7 @@ for (file_num in 1:length(data_files_list)){
     write(paste("# OK: ", tar_file_folder, " - Data Mutations Exists", sep = ""), logfile_path, append=TRUE)
     # Read the data_muations file
     # data_df <- read.table(paste(tar_directory, "data_mutations.txt", sep = "/"), header = TRUE, stringsAsFactors = FALSE, sep = "\t")
-    data_df <- read_tsv(paste(tar_directory, "data_mutations.txt", sep = "/"))
+    data_df <- read_tsv(paste(tar_directory, "data_mutations.txt", sep = "/"), show_col_types = FALSE)
     if("Hugo_Symbol" %in% colnames(data_df) & "Tumor_Sample_Barcode" %in% colnames(data_df)){
       write("# OK: Hugo_Symbol and Tumor_Sample_Barcode Columns exist.", logfile_path, append=TRUE)
       # Select only the columns we need
@@ -114,7 +121,7 @@ for (file_num in 1:length(data_files_list)){
           colnames(samples_df) <- c("Sample_ID", "Cancer Type", "Mutated_Genes")
           ### Find Applicable Drugs
           output_df <- find_applicable_drugs(data_drugs, samples_df)
-          print(output_df)
+          write.table(output_df, file=outputfile_path, sep=";", quote=F, row.names=F, col.names = F, na="NA", append=TRUE)
         }
         else{
           write("# ERROR: Some of the required Columns DO NOT Exist.", logfile_path, append=TRUE)
@@ -133,10 +140,40 @@ for (file_num in 1:length(data_files_list)){
 
 ################################################################################
 
+# Load the Output data
+output_df <- read.csv(outputfile_path, row.names=NULL, sep = ";")
+head(output_df)
+### How Many people for each Diagnosis can be assigned to Drug therapy?
+output_df_1 <- output_df[,c("Sample_ID", "Diagnosis", "Found_Drug")]
+# Some patients may be suitabe for multiple drug therapy, so we remove the duplicated lines
+output_df_1 <- output_df_1[!duplicated(output_df_1),]
+output_df_1 <- subset(output_df_1, select = -c(Sample_ID))
+output_df_1_table <- table(output_df_1)
+output_df_1_table <- cbind(output_df_1_table, rowSums(output_df_1_table))
+output_df_1_table <- rbind(output_df_1_table, c(colSums(output_df_1_table), sum(output_df_1_table)))
+colnames(output_df_1_table) <- c("No Drug Found", "Drug Exists", "Sum")
+rownames(output_df_1_table)[nrow(output_df_1_table)] <- "Sum"
 
-# write(paste("##### Mutations of Diffuse Large B Cell Lymphoma ##### "), output_file_path, append=TRUE)
-# write(paste("### Number of Samples:", length(unique(data_diffuse_large_b_cell_type$Tumor_Sample_Barcode)), sep = " "), output_file_path, append=TRUE)
-# write.fwf(cancer_type_df, file=output_file_path, sep=" ", quote=F, rownames=F, colnames = T, na="NA", append=TRUE)
 
+################################################################################
 
+summary_file <- file(summary_path, open = "w")
 
+title <- "Statistics of Targeted Drugs for Hematological Malignancies"
+subtitle <- "How Many Samples for each Diagnosis can be assigned to the Drug therapy?"
+
+# Create HTML File
+html <- tags$html(
+  tags$body(
+    tags$h1("Statistics of Targeted Drugs for Hematological Malignancies"),
+    tags$h2("How Many Samples for each Diagnosis can be assigned to the Drug therapy?"),
+    htmlTable(output_df_1_table, css.cell = "padding: 0 50px;")
+  )
+)
+
+writeLines(as.character(html), summary_file)
+close(summary_file)
+
+################################################################################
+
+print("DONE!")
